60ade1b195340d210c08548479dc6cb818f47f60
William <william.jiang1101@gmail.com>
2025-10-25 00:02:27 -0400

    Removed my tests

--- src/debug.js
+++ src/debug.js
@@ -13,19 +13,9 @@ const logEachName = (names) => {
   return names.forEach(name => console.log(name, names.indexOf(name), names));
 };
 
-
-const users = [
-  { name: 'Alice', bio: 'Alice is a software engineer' },
-  { name: 'Bob', bio: 'Bob is a teacher' },
-  { name: 'Charlie', bio: 'Charlie is a student' },
-  { name: 'Diana', bio: 'Diana is a doctor' },
-];
-
 const logEachUserBio = (users) => {
   users.forEach((object) => console.log(object.bio));
 };
-
-logEachUserBio(users)
 
 module.exports = {
   myEvery,
